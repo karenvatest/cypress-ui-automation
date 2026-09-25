0c659472c846515ae347bd9ac1b44442b530add2
karenvatest <valdezbetancourtkarengisela@gmail.com>
2026-09-24 19:29:43 -0600

    feat:add new tests cases

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,5 +1,5 @@
 describe("Forms", () =>{
-
+    // TC_Basic_01: Check UI elements of the forms page
     it('Should display form fields correctly', () =>{
         cy.visit('https://www.qapractice.com/practice-forms');
         cy.get('#forms-country').should('be.visible').and('be.enabled');
@@ -15,6 +15,7 @@ describe("Forms", () =>{
         cy.get('#forms-comm-phone').should('be.visible').and('not.be.checked');
     });
 
+    // TC_Basic_02: Verify submit a fully valid form
     it('Should fill the form correctly', () =>{
         cy.visit('https://www.qapractice.com/practice-forms');
         cy.get('#forms-country').select('Other');
@@ -22,17 +23,18 @@ describe("Forms", () =>{
         cy.get('#forms-first-name').type('Giss');
         cy.get('#forms-last-name').type('Luna');
         cy.get('#forms-dob').type('1996-09-24{enter}');
-        cy.get('#forms-doj').type('24-09-2026');
+        cy.get('#forms-doj').type('24/09/2026');
         cy.get('#forms-email').type('gisslun@gmail.com');
         cy.get('#forms-phone-code').select('+Other');
         cy.get('#forms-phone-number').type('2344534551');
         cy.get('#forms-comm-phone').check();
         cy.get('#forms-title').should('have.value', 'Ms.');
-        cy.get('#forms-phone-number').should('have.value', '2344534551');
-        cy.get('#forms-last-name').should('have.value', 'Luna');
         cy.get('#forms-dob').should('have.value', '1996-09-24');
+        cy.get('#forms-submit').click();
+        cy.contains('Details Successfully Added!').should('be.visible');
     });
 
+    // TC_Basic_03: Verify radio buttons are mutually exclusive
     it('Should select phone communication option', () =>{
         cy.visit('https://www.qapractice.com/practice-forms');
         cy.get('#forms-comm-phone').should('not.be.checked');
@@ -40,7 +42,8 @@ describe("Forms", () =>{
         cy.get('#forms-comm-email').should('not.be.checked');
     });
 
-    it.only('Should submit form successfully', () => {
+    // TC_Basic_04: Verify submit an empty form
+    it('Should show required validation messages', () =>{
         cy.visit('https://www.qapractice.com/practice-forms');
         cy.get('#forms-submit').click();
         cy.contains('Country of Residence is required').should('be.visible');
@@ -52,6 +55,49 @@ describe("Forms", () =>{
         cy.contains('Email Address is required').should('be.visible');
         cy.contains('Phone Number is required').should('be.visible');
         cy.contains('Please select a Communication Preference').should('be.visible');
+    });
+
+
+    // TC_Basic_05: Verify Invalid email format
+    it('Should show validation for invalid email format', () =>{
+        cy.visit('https://www.qapractice.com/practice-forms');
+        cy.get('#forms-country').select('Other');
+        cy.get('#forms-title').select('Ms.');
+        cy.get('#forms-first-name').type('Giss');
+        cy.get('#forms-last-name').type('Luna');
+        cy.get('#forms-dob').type('1996-09-24{enter}');
+        cy.get('#forms-doj').type('24/09/2026');
+        cy.get('#forms-email').type('gisslungmail.com');
+        cy.get('#forms-phone-code').select('+Other');
+        cy.get('#forms-phone-number').type('2344534551');
+        cy.get('#forms-comm-phone').check();
+        cy.on('window:alert', (message) => {
+            expect(message).to.equal("Incluye un signo '@' en la direccion de correo electronico. La direccion 'gisslungmail.com' no incluye el signo'@'");
+        }); 
+        cy.get('#forms-submit').click();
+    });
+
+    //TC_Basic_06: Verify Date of joining wrong format
+    it('Should show error for invalid date format', () =>{
+        cy.visit('https://www.qapractice.com/practice-forms');
+        cy.get('#forms-country').select('Other');
+        cy.get('#forms-title').select('Ms.');
+        cy.get('#forms-first-name').type('Giss');
+        cy.get('#forms-last-name').type('Luna');
+        cy.get('#forms-dob').type('1996-09-24{enter}');
+        cy.get('#forms-doj').type('24-09-2026');
+        cy.get('#forms-email').type('gisslun@gmail.com');
+        cy.get('#forms-phone-code').select('+Other');
+        cy.get('#forms-phone-number').type('2344534551');
+        cy.get('#forms-comm-phone').check();
+        cy.get('#forms-submit').click();
+        cy.contains('Date of Joining format should be dd/mm/yyyy').should('be.visible');
+    });
+
+
+    // TC_Basic_07: Verify clear resets the form
+    it('Should clear form fields', () =>{
+        cy.visit('https://www.qapractice.com/practice-forms');
         cy.get('#forms-country').select('Other');
         cy.get('#forms-title').select('Ms.');
         cy.get('#forms-first-name').type('Giss');
@@ -62,7 +108,13 @@ describe("Forms", () =>{
         cy.get('#forms-phone-code').select('+Other');
         cy.get('#forms-phone-number').type('2344534551');
         cy.get('#forms-comm-phone').check();
-        cy.get('#forms-submit').click();
-        cy.contains('Details Successfully Added!').should('be.visible');
+        cy.get('#forms-clear').click();
+        cy.get('#forms-first-name').should('have.value', '');
+        cy.get('#forms-last-name').should('have.value', '');
+        cy.get('#forms-email').should('have.value', '');
+        cy.get('#forms-phone-number').should('have.value', '');
+        cy.get('#forms-comm-phone').should('not.be.checked');
     });
+
+    
 })
